6dd806c0761be8857a7f714c561f25acaed9266e
punyagowda <punyagowdagd@gmail.com>
2026-04-21 17:44:31 +0530

    Added first Java program

---
+++ firstpgrm.java
@@ -0,0 +1,10 @@
+//package practicequestion;
+//This is my first java program
+
+public class firstpgrm{
+    public static void main(String args[])
+    {
+        System.out.println('hello world');
+    }
+}
+    
